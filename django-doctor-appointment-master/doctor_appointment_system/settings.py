ffc4ab72a549b6fb54f5aeda3940e4a3a08a4778
SriramYeluri <nameissriram@gmail.com>
2021-06-09 19:24:56 +0530

    heroku settings

--- django-doctor-appointment-master/doctor_appointment_system/settings.py
+++ django-doctor-appointment-master/doctor_appointment_system/settings.py
@@ -145,3 +145,6 @@ MEDIA_URL = "/media/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import django_heroku
+django_heroku.settings(locals())
